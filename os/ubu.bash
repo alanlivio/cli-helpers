747ea25f1bc65a874c43ccc98248df9e8f4a6e5f
Alan Guedes <a.guedes@reading.ac.uk>
2026-08-11 14:19:11 +0100

    add ubu_install_antigravity

--- os/ubu.bash
+++ os/ubu.bash
@@ -113,6 +113,15 @@ function ubu_install_miniconda() {
     rm -rf ~/bin/miniconda3/miniconda.sh
 }
 
+function ubu_install_antigravity() {
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://us-central1-apt.pkg.dev/doc/repo-signing-key.gpg | \
+    sudo gpg --dearmor --yes -o /etc/apt/keyrings/antigravity-repo-key.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/antigravity-repo-key.gpg] https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/ antigravity-debian main" | \
+    sudo tee /etc/apt/sources.list.d/antigravity.list > /dev/null
+    sudo apt install antigravity
+}
+
 # -- customize --
 
 function gnome_declutter_files() {
