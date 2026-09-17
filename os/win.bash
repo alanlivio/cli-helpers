7c5992018d946c02c36229a2f4c63665f2434140
Alan Guedes <a.guedes@reading.ac.uk>
2026-09-17 15:59:33 +0100

    add wsl_set_cmd_start_as_xdg_open

--- os/win.bash
+++ os/win.bash
@@ -28,6 +28,23 @@ if [[ -n $WSL_DISTRO_NAME ]]; then
             sudo ln -s libcuda.so.1.1 libcuda.so.1
         )
     }
+  
+  
+  function wsl_set_cmd_start_as_xdg_open() {
+    local target_path="/usr/local/bin/xdg-open"
+    if [ -f "$target_path" ]; then return; fi
+    printf '%s\n' \
+        '#!/usr/bin/env bash' \
+        'target="$1"' \
+        '[ -z "$target" ] && exit 0' \
+        'if [ -e "$target" ]; then' \
+        '    target="$(wslpath -w "$target")"' \
+        'fi' \
+        'cmd.exe /c start "" "$target" >/dev/null 2>&1' \
+        | sudo tee "$target_path" > /dev/null
+    sudo chmod +x "$target_path"
+  }
+
 fi
 
 # -- MSYS2 --
