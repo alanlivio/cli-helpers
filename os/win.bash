1f94e465ffa86e0184251cfcf58920a9b8122138
Alan Guedes <a.guedes@reading.ac.uk>
2026-09-19 12:17:17 +0100

    add wsl_fix_path_and_metadata

--- os/win.bash
+++ os/win.bash
@@ -45,6 +45,17 @@ if [[ -n $WSL_DISTRO_NAME ]]; then
     sudo chmod +x "$target_path"
   }
 
+  function wsl_fix_path_and_metadata(){
+    if ! grep -qs 'appendWindowsPath=false' /etc/wsl.conf; then
+        echo "[interop]" >> /etc/wsl.conf
+        echo "appendWindowsPath=false" >> /etc/wsl.conf
+    fi
+    if ! grep -qs 'metadata,umask=0022,fmask=11' /etc/wsl.conf; then
+        echo "[automount]" >> /etc/wsl.conf
+        echo "options=\"metadata,umask=0022,fmask=11\"" >> /etc/wsl.conf
+    fi
+  }
+
 fi
 
 # -- MSYS2 --
